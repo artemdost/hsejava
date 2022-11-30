8cdbf4c15528b24259e502f84f4649119bc03276
artemdost <85932516+artemdost@users.noreply.github.com>
2022-11-30 23:15:34 +0300

    Update lab2_2.java

--- lab2_2.java
+++ lab2_2.java
@@ -5,7 +5,11 @@ public class lab2_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         boolean cond = false;
-        System.out.println("Придумайте надежный пароль: \n" + "* Он должен содержать больше 8 символов \n" + "* Cимволом может быть цифра, английская буква, знак подчеркивания \n" + "* Должен содержать хотя бы одну заглавную букву, одну маленькую букву и одну цифру \n" + "Введите пароль:  ");
+        System.out.println("Придумайте надежный пароль: \n" +
+                "* Он должен содержать больше 8 символов \n" +
+                "* Cимволом может быть цифра, английская буква, знак подчеркивания \n" +
+                "* Должен содержать хотя бы одну заглавную букву, одну маленькую букву и одну цифру \n" +
+                "Введите пароль:  ");
         String s;
         while (cond == false) {
             s = in.nextLine();
@@ -13,7 +17,13 @@ public class lab2_2 {
                 if ((Pattern.compile("[a-z]+").matcher(s).find()) == true) {
                     if ((Pattern.compile("[A-Z]+").matcher(s).find()) == true) {
                         if ((Pattern.compile("[0-9]+").matcher(s).find()) == true) {
-                            cond = true;
+                            if ((Pattern.compile("[а-я]{1,}").matcher(s).find()) == false) {
+                                if ((Pattern.compile("[А-Я]{1,}").matcher(s).find()) == false){
+                                    if ((Pattern.compile("[~`!@#$%^&*()+<>?:№;%:?*/]{1,}").matcher(s).find()) == false){
+                                        cond = true;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
